7d92cf8c801853800e16f92cf2c68f2335bc8623
Davide Nastri <d.nastri@gmail.com>
2016-09-25 18:51:33 +0200

    1st commit

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,16 @@
+package com.example.android.miwok;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by dnastri on 9/25/16.
+ */
+public class WordAdapter extends ArrayAdapter<Word> {
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+
+    }
+}
