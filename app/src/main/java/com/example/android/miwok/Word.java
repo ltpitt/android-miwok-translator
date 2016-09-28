ac6110c3809d7dad5f9c9fbbee5165a88aca90cd
Davide Nastri <d.nastri@gmail.com>
2016-09-28 12:58:14 +0200

    Completed .gitignore implementation

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,32 @@
+package com.example.android.miwok;
+
+/**
+ * Created by dnastri on 9/25/16.
+ */
+public class Word {
+
+
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public void setmDefaultTranslation(String mDefaultTranslation) {
+        this.mDefaultTranslation = mDefaultTranslation;
+    }
+
+    public void setmMiwokTranslation(String mMiwokTranslation) {
+        this.mMiwokTranslation = mMiwokTranslation;
+    }
+}
